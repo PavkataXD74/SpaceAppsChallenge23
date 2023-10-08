8d367504334a3675852f9f18a8b255b55097ad75
Skele-ton <nathanielnechevski@gmail.com>
2023-10-08 12:56:15 +0300

    Add NASA Images Page

--- app.js
+++ app.js
@@ -6,20 +6,29 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const imagesRoot = "https://images-api.nasa.gov/search?keywords=";
-const imageTitles = [
-    "mercury,globe,south,pole",
-    "capturing,venus,transit,from,iss",
-    "earth",
-    "mars,2003",
-    "composite,image,of,the,planet,mars,taken,by,hubble,space,telescope",
-    "earth-based,observation,of,jupiter",
-    "saturn,with,rhea,and,dione,false,color",
-    "keck,telescope,views,of,uranus",
-    "neptune",
-    "earth,moon",
-    "europa"
-]
+const imagesRoot = "https://images-api.nasa.gov/asset/";
+const imageTitles = {
+    "Spotless Sun": "PIA21569",
+    "Mercury Globe South Pole": "PIA15164",
+    "Capturing Venus Transit from ISS": "GSFC_20171208_Archive_e001747",
+    "Earth": "PIA18033",
+    "Earth Moon": "PIA00405",
+    "Mars 2003": "PIA04591",
+    "Earth-based Observation of Jupiter": "PIA22936",
+    "Saturn with Rhea and Dione false color": "PIA01143",
+    "Uranus - Discrete Cloud": "PIA00370",
+    "Neptune": "PIA01998",
+};
+
+const horizonsStart = "https://ssd.jpl.nasa.gov/api/horizons.api?COMMAND='";
+const horizonsEnd = "'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='@sun'&START_TIME='2023-10-08'&STOP_TIME='2023-10-09'&STEP_SIZE='1h'&QUANTITIES='1,9,20,23,24,29'&format=text"
+const commandNumber = ["1"];
+
+const monthNames = [
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec"
+];
 
 app.set("view engine", "ejs");
 
@@ -27,18 +36,67 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("earth");
+app.get("/size", (req, res) => {
+    res.render("index", { page: "horizontal.ejs", nasaData: [] });
+});
 
-    // axios.all(imageTitles.map((title, index) => axios.get(`${imagesRoot}${title}`)))
-    // axios.get(`${imagesRoot}mercury`)
-    // .then(response => {
-    //     const randomImage = response.data.collection.items[Math.floor(Math.random() * 100)];
-    //     console.log(randomImage.data);
-    //     console.log("\n\n\n\n");
-    //     res.render("main", {data: randomImage.links[0].href});
-    // })
-    // .catch(err => console.log(err));
+app.get("/location", (req, res) => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = monthNames[currentDate.getMonth()];
+    const day = currentDate.getDate();
+
+    const hour = currentDate.getHours().toString().padStart(2, '0');
+
+    const formattedDate = ` ${year}-${month}-${day.toString().padStart(2, '0')}`;
+
+    axios.all(commandNumber.map(command => axios.get(`${horizonsStart}${command}${horizonsEnd}`)))
+    .then(response => {
+        const degrees= [];
+        for(let i = 0; i < response.length; i++) {
+            const splitResponse = response[i].data.split("\n");
+            let actualData;
+            
+            for(let i = 0; i <  splitResponse.length; i++) {
+                if(splitResponse[i].startsWith(formattedDate) && splitResponse[i].substr(13, 2) === hour) {
+                    actualData = splitResponse[i];
+                }
+            }
+            const xInMintues = actualData.substr(23, 5);
+            const yInMintues = actualData.substr(35, 6);
+            const x = Number(xInMintues.substr(0, 2)) + (Number(xInMintues.substr(3,2)) / 60) + (Number(xInMintues.substr(5,4)) / 3600);
+            const y = Number(yInMintues.substr(0, 3)) + (Number(yInMintues.substr(4,3)) / 60) + (Number(yInMintues.substr(7,3)) / 3600);
+            let degree = (x / y) * (180 / 3.14);
+
+            if(degree < 0) {
+                degree = 360 + degree;
+            }
+
+            degrees.push(degree);
+        }
+
+        res.render("index", { page: "top_down.ejs", nasaData: [degrees] });
+    })
+    .catch(err => console.log(err.response.data));
+})
+
+app.get("/nasa-images", (req, res) => {
+    const imageLinks = [];
+
+    axios.all(Object.values(imageTitles).map(id => axios.get(`${imagesRoot}${id}`)))
+    .then(response => {
+        for(let i = 0; i < response.length; i++) {
+            const nasaImage = response[i].data.collection.items.at(-2); 
+            imageLinks.push(nasaImage.href);
+        }
+
+        res.render("index", { page: "horizontal.ejs", nasaData: imageLinks });
+    })
+    .catch(err => console.log(err));
+})
+
+app.get(["/", "/size"], (req, res) => {
+    res.redirect("/size");
 });
 
 app.listen(PORT, () => {
